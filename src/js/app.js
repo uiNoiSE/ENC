931120ae03ccb7ed4cf2e7efb74e72eb8db0658b
Minaev Vlad <uiNoiSE@gmail.com>
2021-02-19 16:33:32 +0300

    Add: - libs for svg animations - animated "blobs" to all sections

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
 import "../scss/app.scss";
 
+import { spline } from "@georgedoescode/spline";
+import SimplexNoise from "simplex-noise";
+
 import "./main.js";
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,83 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+import { spline } from "@georgedoescode/spline";
+import SimplexNoise from "simplex-noise";
 
 AOS.init();
+
+// our <path> element
+var path = document.querySelector('.blob-parent > path');
+
+let noiseStep = 0.003;
+
+const simplex = new SimplexNoise();
+
+const points = createPoints();
+
+(function animate() {
+  path.setAttribute("d", spline(points, 1, true));
+
+  // for every point...
+  for (let i = 0; i < points.length; i++) {
+    const point = points[i];
+
+    // return a pseudo random value between -1 / 1 based on this point's current x, y positions in "time"
+    const nX = noise(point.noiseOffsetX, point.noiseOffsetX);
+    const nY = noise(point.noiseOffsetY, point.noiseOffsetY);
+    // map this noise value to a new value, somewhere between it's original location -20 and it's original location + 20
+    const x = map(nX, -1, 1, point.originX - 20, point.originX + 20);
+    const y = map(nY, -1, 1, point.originY - 20, point.originY + 20);
+
+    // update the point's current coordinates
+    point.x = x;
+    point.y = y;
+
+    // progress the point's x, y values through "time"
+    point.noiseOffsetX += noiseStep;
+    point.noiseOffsetY += noiseStep;
+  }
+
+
+  requestAnimationFrame(animate);
+})();
+
+function map(n, start1, end1, start2, end2) {
+  return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2;
+}
+
+function noise(x, y) {
+  return simplex.noise2D(x, y);
+}
+
+function createPoints() {
+  const points = [];
+  // how many points do we need
+  const numPoints = 6;
+  // used to equally space each point around the circle
+  const angleStep = (Math.PI * 2) / numPoints;
+  // the radius of the circle
+  const rad = 80;
+
+  for (let i = 1; i <= numPoints; i++) {
+    // x & y coordinates of the current point
+    const theta = i * angleStep;
+
+    const x = 100 + Math.cos(theta) * rad;
+    const y = 100 + Math.sin(theta) * rad;
+
+    // store the point's position
+    points.push({
+      x: x,
+      y: y,
+      // we need to keep a reference to the point's original point for when we modulate the values later
+      originX: x,
+      originY: y,
+      // more on this in a moment!
+      noiseOffsetX: Math.random() * 1000,
+      noiseOffsetY: Math.random() * 1000,
+    });
+  }
+
+  return points;
+}
